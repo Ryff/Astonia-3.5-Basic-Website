f32e759125b5cb3725921245f5bc69c92c444548
CPG-Chris <Ryff@users.noreply.github.com>
2019-03-07 01:34:24 -0500

    Update config.php

--- config.php
+++ config.php
@@ -10,8 +10,11 @@ $analytics = "UA-51401830-13"; // Google analytics code, to keep track of people
 $mysql_hostname = 'localhost';
 $mysql_username = 'root';
 $mysql_password = '';
-$mysql_dbname = 'merc1';
+$mysql_dbname = 'merc0';
+$mysql_db2name = 'merc1';
 $dbh = new PDO("mysql:host=$mysql_hostname;dbname=$mysql_dbname", $mysql_username, $mysql_password);
+$dbh2 = new PDO("mysql:host=$mysql_hostname;dbname=$mysql_db2name", $mysql_username, $mysql_password);
+
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
